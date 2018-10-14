f2ddfca60326c6fca53509819c2e50805cdb8a4e
matheus-lima92 <lima.eca@gmail.com>
2018-10-14 17:59:03 -0300

    cors enabled

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const index = require('./routes/index');
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
